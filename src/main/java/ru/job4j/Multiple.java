e625c4a6d3f6382b14ec6c10b51e1d6a146e2f96
bulyts01 <bulytovs@mail.ru>
2022-01-07 17:37:32 +0800

    3.2. Загрузка файлов на github [#243562] 3.3. Фиксация изменений на github [#243564] 6. Checkstyle

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println("1*1=1");
+        System.out.println("1*2=2");
+        System.out.println("1*3=6");
+        System.out.println("1*4=8");
+        System.out.println("1*5=10");
+        System.out.println("1*6=12");
+        System.out.println("1*7=14");
+        System.out.println("1*8=16");
+        System.out.println("1*9=18");
+        System.out.println("1*10=20");
+    }
+}
