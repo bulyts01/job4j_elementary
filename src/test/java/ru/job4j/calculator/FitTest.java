e679936e9da44e368e65719dc38f344b3e9b794e
bulyts01 <bulytovs@mail.ru>
2022-01-09 16:24:50 +0800

    2. Тесты для задачи "идеальный вес" [#159458 #248375]

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -16,7 +16,7 @@ public class FitTest {
     }
 
     @Test
-    public void whenWoman180Then80() {
+    public void whenWoman180Then80dot5() {
         short in = 180;
         double expected = 80.5;
         double out = Fit.womanWeight(in);
