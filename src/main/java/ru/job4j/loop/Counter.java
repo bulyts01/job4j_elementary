7ab233b55e9a8b2bd8411aa8e91c800d4ec39144
bulyts01 <bulytovs@mail.ru>
2022-01-16 20:37:03 +0800

    5.1. Подсчет суммы чётных чисел в диапазоне [#192 #251524]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -11,9 +11,9 @@ public class Counter {
 
     public static int sumByEven(int start, int finish) {
         int sum = 0;
-        for (int i = start; i <= finish; i++) {
-            if (i % 2 == 0) {
-                sum = sum + i;
+        for (int l = start; l <= finish; l++) {
+            if (l % 2 == 0) {
+                sum = sum + l;
             }
         }
         return sum;
