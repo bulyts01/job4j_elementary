44f64bacd2d2e1eec24f0ae59218e803fc70be9a
bulyts01 <bulytovs@mail.ru>
2022-01-29 17:00:01 +0800

    6.8. Дефрагментация массива [#225 #256659]

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -15,9 +15,8 @@ public class Defragment {
             if (array[index] == null) {
                 for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
-                        String buffer = array[i];
-                        array[i] = array[index];
-                        array[index] = buffer;
+                        array[index] = array[i];
+                        array[i] = null;
                         break;
                     }
                 }
