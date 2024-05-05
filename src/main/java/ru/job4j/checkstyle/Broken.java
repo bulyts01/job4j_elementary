4fa7b3e7cef830821b955f65895508ecd18346f9
Semen <bulytovs@mail.ru>
2024-05-05 12:13:07 +0800

    6. Правила Checkstyle [#504981 #494375]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -3,7 +3,7 @@ package ru.job4j.checkstyle;
 public class Broken {
     private int sizeOfEmpty = 10;
     private String surname;
-    private static final String NEW_VALUE = "";
+    private static final String NEW_VALUE = " ";
     private String name;
 
     public Broken() { }
